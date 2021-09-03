42091df79bffb336ddf2918d53e7e68367f6aa27
mcfaith9 <marclouie.cabigas@yahoo.com>
2021-09-03 21:45:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# quizvue
+
+Build a Quiz App with Vue and Tailwind.
+An intermediate level project for learning vue
+
+**You would learn**
+
+- Setting up Vue with tailwind
+- Design the Quiz app UI
+- Dynamically change classes based on conditions
+- Fetch data from API
+
+## 1. Create and Style the User Interface. Part 1(A)
+
+In this video I would cover the design part of the quiz app, using tailwind. Starting with the tailwind set up.
+
+## 1b. Create and Style the User Interface. Part 1(B)
+
+In this video I would cover the design part of the quiz app, using tailwind. Applying custom styles to carter to different conditions. EG when the option is wrong, correct or the default state.
+
+**Resources**
+
+- [Tailwindcss](https://tailwindcss.com/)
+- [Vue CLI](https://cli.vuejs.org)
+
+_Would update as I upload more series_
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,117 @@
+<template>
+  <main class="flex h-screen items-center justify-center bg-gray-100">
+    <!-- quiz container -->
+
+    <div class="overflow-hidden bg-white flex-none container relative shadow-lg rounded-lg px-12 py-6">
+      <img src="@/assets/images/abstract.svg" alt="" lass="absolute -top-10 left-0 object-none"/>
+
+      <!-- contents -->
+      <div class="relative z-20">
+        <!-- score container -->
+        <div class="text-right text-gray-800">
+          <p class="text-sm leading-3">Score</p>
+          <p class="font-bold">60</p>
+        </div>
+
+        <!-- timer container -->
+        <div class="bg-white shadow-lg p-1 rounded-full w-full h-5 mt-4">
+          <div class="bg-blue-700 rounded-full w-11/12 h-full"></div>
+        </div>
+
+        <!-- question container -->
+        <div class="rounded-lg bg-gray-100 p-2 neumorph-1 text-center font-bold text-gray-800 mt-8">
+          <div class="bg-white p-5">
+            {{ currentQuestion.question }}
+          </div>
+        </div>
+
+        <div class="mt-8">
+          <div v-for="(choice, item) in currentQuestion.choices" :key="item">
+            <div class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative" :ref="optionChosen" @click="onOptionClicked(choice, item)">
+              <div class="bg-blue-700 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 top-0 shadow-md">
+                <p class="transform -rotate-45">+10</p>
+              </div>
+              <div class="rounded-lg font-bold flex p-2">
+                <div class="p-3 rounded-lg">{{item}}</div>
+                <div class="flex items-center pl-6">{{ choice }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- progress indicator container -->
+        <div class="mt-8 text-center">
+          <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
+          <p class="font-bold text-gray-800">2/10</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+ <style scoped>
+.neumorph-1 {
+  box-shadow: 6px 6px 18px rgba(0, 0, 0, 0.09), -6px -6px 18px #ffffff;
+}
+.container {
+  max-width: 400px;
+  border-radius: 25px;
+}
+</style>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let questionCounter = ref(0);
+    const currentQuestion = ref({
+      question: "",
+      answer: 1,
+      choices: []
+    });
+
+    const questions = [
+      {
+        question: "When coughing or sneezing? What sould you do?",
+        answer: 1,
+        choices: ["Cover your mouth and nose with flexed elbow or tissue","Its no problem at all","Just continue sneezing so you can get rid of virus"],
+      },
+      {
+        question: "After going out what should you do?",
+        answer: 2,
+        choices: ["Wash your hand soap and water, or an alcohol","Just ignore it", "I dont need to wash"],
+      }
+    ];
+
+    const onQuizStart = () => {
+      currentQuestion.value = questions[questionCounter.value];
+    };
+
+    let itemsRef = [];
+    const optionChosen = (element) => {   
+      if(element) {
+        itemsRef.push(element);
+      }
+    };
+
+    const onOptionClicked = (choice, item) => {
+      const divContainer = itemsRef[item];
+      const optionID = item + 1;
+      if(currentQuestion.value.answer == optionID) {
+        divContainer.classList.add("option-correct");
+        divContainer.classList.remove("option-default");
+      } else {
+        divContainer.classList.add("option-wrong");
+        divContainer.classList.remove("option-default");
+      }      
+    };
+
+    return { currentQuestion, questions, questionCounter, onQuizStart, onOptionClicked, optionChosen};
+  },
+  methods: {
+  },
+  mounted() {
+    this.onQuizStart();
+  }
+};
+</script> 
